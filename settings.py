7d72786a0039f2122288f1cbb3266e1815da112d
GennosukeKoita <69233876+GennosukeKoita@users.noreply.github.com>
2022-06-13 12:20:20 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ paths defined here are used in many places
 
 DATA_PATH = '../datasets' # magicpoint export
 # DATA_PATH = 'datasets' # magicpoint training
-EXPER_PATH = 'logs' # path for saving checkpoints
+EXPER_PATH = '../logs' # path for saving checkpoints
 SYN_TMPDIR = './datasets/'  # path for dumping synthetic data
 DEBUG = False # true: will make synthetic data only uses draw_checkboard and ignore other classes
 # DEBUG = False
